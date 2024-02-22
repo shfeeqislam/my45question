f8668eade086834dcda606be038099551a8a0188
shafeeq <159651588+shfeeqislam@users.noreply.github.com>
2024-02-22 14:21:44 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var message = "hello world";
+console.log(message);
+// question 2
+var hello = "hello atta would you like some python practice today";
+console.log(hello);
+// question 3 lower case
+var personName = "shafeeq";
+console.log(personName.toLocaleLowerCase());
+// uppercase
+console.log(personName.toUpperCase());
+var quote = "I've learned that people you forget what you said, people will forget what you did ,but people will never forget how you made them feel.";
+var author = "Maya Angelou";
+console.log("".concat(author, " once said, \"").concat(quote));
---
+++ main.ts
@@ -0,0 +1,18 @@
+let message : string = "hello world";
+console.log(message);
+// question 2
+let hello :string ="hello atta would you like some python practice today";
+console.log(hello);
+// question 3 lower case
+const personName :string = 
+"shafeeq";
+console.log(personName.toLocaleLowerCase(
+));
+// uppercase
+console.log(personName.toUpperCase(
+))
+
+const quote: string = "I've learned that people you forget what you said, people will forget what you did ,but people will never forget how you made them feel.";
+const author: string = "Maya Angelou";
+
+console.log(`${author} once said, "${quote}`);
